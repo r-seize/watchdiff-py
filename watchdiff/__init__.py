022a171b6eab34ba4eee033a136ef85574f7a481
r-seize <riynies@gmail.com>
2026-05-05 22:56:49 +0200

    refactor: wire all new features into WatchDiff core

--- watchdiff/__init__.py
+++ watchdiff/__init__.py
@@ -1,3 +1,14 @@
 from watchdiff.core import WatchDiff
+from watchdiff.exporter import Exporter
+from watchdiff.models import BrowserOptions, DiffMode, WatchConfig
+from watchdiff.store import SqliteStore, Store
 
-__all__ = ["WatchDiff"]
+__all__ = [
+    "BrowserOptions",
+    "DiffMode",
+    "Exporter",
+    "SqliteStore",
+    "Store",
+    "WatchConfig",
+    "WatchDiff",
+]
